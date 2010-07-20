aa3d42c266366cb8c6ea7c7ce11e6dd9e81e9c84
Kerry Buckley <kerryjbuckley@gmail.com>
2010-07-20 13:22:00 +0100

    Failing test for n numbers

--- StringCalculator.hs
+++ StringCalculator.hs
@@ -1,10 +1,13 @@
 import Test.QuickCheck
+import Data.List
 
 prop_empty_sum_is_zero = add "" == 0
 
 prop_sum_of_one_number_is_itself a = add (show a) == a
 
 prop_sum_two_numbers a b = add (show a ++ "," ++ show b) == a + b
+
+prop_sum_n_numbers numbers = add (intercalate "," (map show numbers)) == sum numbers
 
 add :: String -> Integer
 add "" = 0
