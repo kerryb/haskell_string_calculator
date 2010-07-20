61dd884765f952dd1ff19ad4121acfd1ca833554
Kerry Buckley <kerryjbuckley@gmail.com>
2010-07-20 13:12:27 +0100

    Handle a single number

--- StringCalculator.hs
+++ StringCalculator.hs
@@ -2,10 +2,14 @@ import Test.QuickCheck
 
 prop_empty_sum_is_zero = add "" == 0
 
+prop_sum_of_one_number_is_itself a = add (show a) == a
+
 prop_sum_two_numbers a b = add (show a ++ "," ++ show b) == a + b
 
+add :: String -> Integer
 add "" = 0
-add xs = read a + read b
+add xs | any (== ',') xs = read a + read b
+       | otherwise  = read xs
   where
     (a, b) = (a', tail b')
     (a', b') = break (== ',') xs
