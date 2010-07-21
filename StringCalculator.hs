5a44c18ee630d3d9c19cc9f80d3a3e8ee9e604f9
Kerry Buckley <kerryjbuckley@gmail.com>
2010-07-21 11:36:26 +0100

    Special case for empty string no longer required

--- StringCalculator.hs
+++ StringCalculator.hs
@@ -10,7 +10,6 @@ prop_sum_two_numbers a b = add (show a ++ "," ++ show b) == a + b
 prop_sum_n_numbers numbers = add (intercalate "," (map show numbers)) == sum numbers
 
 add :: String -> Integer
-add "" = 0
 add xs = sum (extractNumbers xs) where
   extractNumbers xs = map read (split ',' xs)
   split _ [] = []
